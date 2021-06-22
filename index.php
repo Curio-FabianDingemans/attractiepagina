6b7546b51f971f9f791c1be4bc18c87c6d3c0c5f
Faab007NL <mail@fabiandingemans.nl>
2021-06-22 10:43:54 +0200

    added filter options

--- index.php
+++ index.php
@@ -1,6 +1,31 @@
 <?php
 session_start();
 require_once 'admin/backend/config.php';
+
+$themeland = null;
+$minlength = null;
+$fastpass = "";
+
+if(isset($_GET['themeland'])){
+    $themeland = $_GET['themeland'];
+}
+if(isset($_GET['minlength'])){
+    $minlength = $_GET['minlength'];
+    if($minlength < 0){
+        $minlength = 0;
+    }
+    if($minlength > 300){
+        $minlength = 300;
+    }
+}
+if(isset($_GET['fastpass'])){
+    if($_GET['fastpass'] == "0"){
+        $fastpass = 0;
+    }else if($_GET['fastpass'] == "1"){
+        $fastpass = 1;
+    }
+}
+
 ?>
 
 <!doctype html>
@@ -20,14 +45,90 @@ require_once 'admin/backend/config.php';
     <?php require_once 'header.php'; ?>
     <div class="container content">
         <aside>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia modi dolore magnam! Iste libero voluptatum autem, sapiente ullam earum nostrum sed magnam vel laboriosam quibusdam, officia, esse vitae dignissimos nulla?
+            <form action="./" method="get">
+                <div class="attractieSort">
+                    <label for="themeland">Themeland:</label>
+                    <select name="themeland" id="themeland">
+                        <option value="" <?php echo $themeland==null ? "selected" : "" ?>> - Sorteer op themaland - </option>
+                        <option value="familyland" <?php echo $themeland=="familyland" ? "selected" : "" ?>>Familyland</option>
+                        <option value="waterland" <?php echo $themeland=="waterland" ? "selected" : "" ?>>Waterland</option>
+                        <option value="adventureland" <?php echo $themeland=="adventureland" ? "selected" : "" ?>>Adventureland</option>
+                    </select>
+                </div>
+                <div class="attractieSort">
+                    <label for="minlength">Minimale Lengte:</label>
+                    <input type="number" name="minlength" id="minlength" value="<?php echo $minlength ?>" placeholder="Sorteer op minimale lengte">
+                </div>
+                <div class="attractieSort">
+                    <label for="fastpass">Fastpass:</label>
+                    <select name="fastpass" id="fastpass">
+                        <option value="" <?php echo $fastpass=="" ? "selected" : "" ?>> - Sorteer op fastpass - </option>
+                        <option value="1" <?php echo $fastpass===1 ? "selected" : "" ?>>Ja</option>
+                        <option value="0" <?php echo $fastpass===0 ? "selected" : "" ?>>Nee</option>
+                    </select>
+                </div>
+                <div class="attractieSort">
+                    <button type="submit">Filter toepassen</button>
+                </div>
+            </form>
         </aside>
         <main>
             <?php
                 require_once 'admin/backend/conn.php';
-                $query = "SELECT * FROM rides";
-                $statement = $conn->prepare($query);
-                $statement->execute();
+                
+                if(!empty($themeland) && ($fastpass === 0 || $fastpass === 1)){
+                    $query = "SELECT * FROM rides WHERE `themeland`=:themeland And `fast_pass`=:fast_pass;";
+                    $statement = $conn->prepare($query);
+                    $statement->execute([
+                        "themeland" => $themeland,
+                        "fast_pass" => $fastpass
+                    ]);
+                }else if(!empty($themeland) && !empty($minlength)){
+                    $query = "SELECT * FROM rides WHERE `themeland`=:themeland And `min_length`>=:min_length;";
+                    $statement = $conn->prepare($query);
+                    $statement->execute([
+                        "themeland" => $themeland,
+                        "min_length" => $minlength
+                    ]);
+                }else if(!empty($minlength) && ($fastpass === 0 || $fastpass === 1)){
+                    $query = "SELECT * FROM rides WHERE `min_length`>=:min_length And `fast_pass`=:fast_pass;";
+                    $statement = $conn->prepare($query);
+                    $statement->execute([
+                        "min_length" => $minlength,
+                        "fast_pass" => $fastpass
+                    ]);
+                }else if(!empty($themeland) && !empty($minlength) && ($fastpass === 0 || $fastpass === 1)){
+                    $query = "SELECT * FROM rides WHERE `themeland`=:themeland And `min_length`>=:min_length And `fast_pass`=:fast_pass;";
+                    $statement = $conn->prepare($query);
+                    $statement->execute([
+                        "themeland" => $themeland,
+                        "min_length" => $minlength,
+                        "fast_pass" => $fastpass
+                    ]);
+                }else if(!empty($themeland)){
+                    $query = "SELECT * FROM rides WHERE `themeland`=:themeland;";
+                    $statement = $conn->prepare($query);
+                    $statement->execute([
+                        "themeland" => $themeland
+                    ]);
+                }else if(!empty($minlength)){
+                    $query = "SELECT * FROM rides WHERE `min_length`>=:min_length;";
+                    $statement = $conn->prepare($query);
+                    $statement->execute([
+                        "min_length" => $minlength
+                    ]);
+                }else if($fastpass == 0 || $fastpass == 1){
+                    $query = "SELECT * FROM rides WHERE `fast_pass`=:fast_pass;";
+                    $statement = $conn->prepare($query);
+                    $statement->execute([
+                        "fast_pass" => $fastpass
+                    ]);
+                }else{
+                    $query = "SELECT * FROM rides";
+                    $statement = $conn->prepare($query);
+                    $statement->execute();
+                }
+                
                 $rides = $statement->fetchAll(PDO::FETCH_ASSOC);
             ?>
             <div class="atracties">
